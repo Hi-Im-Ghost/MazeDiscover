f5de96f769ceb568bac6c3d12ddf21061f6d9717
Adrian Chmielowiec <adikx107@gmail.com>
2023-11-15 15:26:25 +0100

    Add config and change env

--- Assets/Scripts/MoveToTargetAgent.cs
+++ Assets/Scripts/MoveToTargetAgent.cs
@@ -13,28 +13,30 @@ public class MoveToTargetAgent : Agent
 
     public override void OnEpisodeBegin()
     {
-        transform.localPosition = new Vector3(Random.Range(-3.5f,-1.5f),0f, Random.Range(-3.5f, 3.5f));
-        target.localPosition = new Vector3(Random.Range(1.5f, 3.5f), 0f, Random.Range(-3.5f, 3.5f));
+        transform.localPosition = new Vector3(Random.Range(-3.5f,-1.5f), 0.56f, Random.Range(-3.5f, 3.5f));
+        target.localPosition = new Vector3(Random.Range(1.5f, 3.5f), 0.56f, Random.Range(-3.5f, 3.5f));
         
         env.rotation = Quaternion.Euler(0, Random.Range(0, 360f), 0);
         transform.rotation = Quaternion.identity;
+        
     }
+    //Metoda do okreslenia celu jaki ma osiagnac AI
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation((Vector3)transform.localPosition);
-        sensor.AddObservation((Vector3)target.localPosition);
+        sensor.AddObservation((Vector3)transform.localPosition); //pozycja AI
+        sensor.AddObservation((Vector3)target.localPosition); //pozycja celu
     }
-
+    //Metoda do okreslenia jakich akcji ma sie podjac AI by osiagnac cel
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
+        float moveX = actions.ContinuousActions[0];  //akcja przesuniecia sie po X
+        float moveZ = actions.ContinuousActions[1]; //akcja przesuniecia sie po Y
 
         float movementSpeed = 5f;
 
         transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * movementSpeed;
     }
-
+    //Metoda ktora pozwala uzytkownikowi sterowac dzialaniami w celu testu 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continousActions = actionsOut.ContinuousActions;
